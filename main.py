801426f0298b5df8585038c2ae6adb5230883191
Deepak Ganesan <gdeepak.iitm@gmail.com>
2023-04-01 22:12:33 +0530

    add argparse

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 """ This is my first code in pycharm. I shall be setting up a package that can be run from the command line.
 This shall also include logging functions and will be linked to a remote repo."""
 
+import argparse
 import preprocess.analyze as ace
 
 if __name__ == "__main__":
-    analyzer = ace.Analyze("./data/example.csv")
-    analyzer.column_sum()
+    parser = argparse.ArgumentParser(description="Analyze any pandas dataframe")
+    parser.add_argument("-x", "--xvar", type=int, help="1 for analyzing")
+    args = parser.parse_args()
+    if args.xvar == 1:
+        analyzer = ace.Analyze("./data/example.csv")
+        analyzer.column_sum()
