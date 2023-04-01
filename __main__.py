8fd6ef35c6e486aaf25498c94bfb8f5f38197584
Deepak Ganesan <gdeepak.iitm@gmail.com>
2023-04-01 23:57:18 +0530

    use yaml to load config

---
+++ __main__.py
@@ -0,0 +1,6 @@
+""" This is my first code in pycharm. I shall be setting up a package that can be run from the command line.
+This shall also include logging functions and will be linked to a remote repo."""
+from .cli import cli
+
+if __name__ == "__main__":
+    cli()
---
+++ cli.py
@@ -0,0 +1,15 @@
+import argparse
+import yaml
+
+with open(r"first\configs\input.yaml", "rb") as f:
+    config = yaml.safe_load(f)
+
+import first.preprocess.analyze as ace
+
+def cli():
+    parser = argparse.ArgumentParser(description="Analyze any pandas dataframe")
+    parser.add_argument("-x", "--xvar", type=int, help="1 for analyzing")
+    args = parser.parse_args()
+    if args.xvar == 1:
+        analyzer = ace.Analyze(config['data']['input'])
+        analyzer.column_sum()
